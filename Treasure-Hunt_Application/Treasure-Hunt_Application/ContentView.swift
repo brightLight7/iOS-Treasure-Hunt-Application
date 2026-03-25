b21c4d3aa91058d231e856c017aa5d8712640be0
reda-ej <reda.ejhani@gmail.com>
2026-03-25 11:52:28 +0000

    Initial Application Made

---
+++ Treasure-Hunt_Application/Treasure-Hunt_Application/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Treasure-Hunt_Application
+//
+//  Created by Reda Ejhani on 25/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
