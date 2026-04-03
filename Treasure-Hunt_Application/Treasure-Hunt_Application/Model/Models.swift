4fd5437a4a456497cbe1f993851fc8fe35eb1a19
Abdullah Sajid <mrabdullahsajid88@gmail.com>
2026-04-03 12:33:52 +0100

    Update Models.swift
    - Added the Event struct to define the event entity

--- Treasure-Hunt_Application/Treasure-Hunt_Application/Model/Models.swift
+++ Treasure-Hunt_Application/Treasure-Hunt_Application/Model/Models.swift
@@ -94,3 +94,51 @@ struct User: Codable, Identifiable
         try container.encode(userImageURL ?? "https://placehold.co/300x300/png", forKey: .userImageURL)
     }
 }
+
+// MARK: - Event
+
+struct Event: Codable, Identifiable
+{
+    let eventID: FlexibleID
+    var eventName: String
+    var eventDescription: String
+    var eventOwnerID: FlexibleID
+    var eventIsPublic: Bool
+    var eventStart: String
+    var eventFinish: String
+    var eventStatusID: FlexibleID
+    var eventOwner: User?
+    var eventStatus: Status?
+    
+    var id: String {eventID.value}
+    var startDate: Date? {ISO8601DateFormatter().date(from: eventStart)}
+    var finishDate: Date? {ISO8601DateFormatter().date(from: eventFinish)}
+    
+    enum CodingKeys: String, CodingKey
+    {
+        case eventID = "EventID"
+        var eventName = "eventName"
+        var eventDescription = "eventDescription"
+        var eventOwnerID = "eventOwnerID"
+        var eventIsPublic = "eventIsPublic"
+        var eventStart = "eventStart"
+        var eventFinish = "eventFinish"
+        var eventStatusID = "eventStatusID"
+        var eventOwner = "eventOwner"
+        var eventStatus = "eventStatus"
+    }
+    
+    func encode(to encoder: Encoder) throws
+    {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        if !eventID.isNew { try container.encode(eventID, forKey: .eventID) }
+        
+        try container.encode(eventName, forKey: .eventName)
+        try container.encode(eventDescription, forKey: .eventDescription)
+        try container.encode(eventOwnerID, forKey: .eventOwnerID)
+        try container.encode(eventIsPublic, forKey: .eventIsPublic)
+        try container.encode(eventStart, forKey: .eventStart)
+        try container.encode(eventFinish, forKey: .eventFinish)
+        try container.encode(eventStatusID, forKey: .eventStatusID)
+    }
+}
