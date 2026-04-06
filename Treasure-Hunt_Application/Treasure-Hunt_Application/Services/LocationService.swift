3c93177714239c9f38115705652b5592b6c6a506
reda-ej <reda.ejhani@gmail.com>
2026-04-06 14:44:54 +0100

    Add location service functionality
    - Implemented location services for the treasure hunt app.

---
+++ Treasure-Hunt_Application/Treasure-Hunt_Application/Services/LocationService.swift
@@ -0,0 +1,57 @@
+//
+//  LocationService.swift
+//  Treasure-Hunt_Application
+//
+//  Created by Reda Ejhani on 06/04/2026.
+//
+
+import Foundation
+import CoreLocation
+import Combine
+
+// MARK: - LocationService
+
+final class LocationService: NSObject, ObservableObject {
+    
+    @Published var userLocation: CLLocation?
+    @Published var authorizationStatus: CLAuthorizationStatus = .notDetermined
+    @Published var heading: CLHeading?
+    
+    private let manager = CLLocationManager()
+    
+    static let proximityThreshold: CLLocationDistance = 30
+    
+    override init() {
+        super.init()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.distanceFilter = 5
+        manager.headingFilter = 5
+    }
+    
+    func requestPermission() {
+        manager.requestWhenInUseAuthorization()
+    }
+    
+    func startUpdating() {
+        manager.startUpdatingLocation()
+        manager.stopUpdatingHeading()
+    }
+    
+    func stopUpdating() {
+        manager.stopUpdatingLocation()
+        manager.stopUpdatingHeading()
+    }
+    
+    func distance(to cache: Cache) -> CLLocationDistance? {
+        guard let userLocation else { return nil }
+        let cacheLocation = CLLocation(latitude: cache.cacheLatitude, longitude: cache.cacheLongitude)
+        return userLocation.distance(from: cacheLocation)
+        
+        func isNearby(cache: Cache) -> Bool {
+            guard let dist = distance(to: cache) else { return false }
+            return dist <= Self.proximityThreshold
+        }
+    }
+}
+
