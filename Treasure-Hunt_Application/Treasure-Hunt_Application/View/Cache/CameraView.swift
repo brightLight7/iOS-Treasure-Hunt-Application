e862637c8ca932a4a96c3e71b2dd9bc2a394536b
reda-ej <reda.ejhani@gmail.com>
2026-04-07 20:18:24 +0100

    Finished CameraView.swift
    - Implemented Coordinator delegate

--- Treasure-Hunt_Application/Treasure-Hunt_Application/View/Cache/CameraView.swift
+++ Treasure-Hunt_Application/Treasure-Hunt_Application/View/Cache/CameraView.swift
@@ -22,4 +22,29 @@ struct CameraView: UIViewControllerRepresentable {
     }
     
     func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+    
+    // MARK: - Coordinator (delegate)
+    
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        let parent: CameraView
+        
+        init(_ parent: CameraView) { self.parent = parent }
+        
+        func imagePickerController(_ picker: UIImagePickerController,
+                                   didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+            if let image = info[.originalImage] as? UIImage {
+                let filename = UUID().uuidString + ".jpg"
+                let url = FileManager.default.temporaryDirectory.appendingPathComponent(filename)
+                if let data = image.jpegData(compressionQuality: 0.8) {
+                    try? data.write(to: url)
+                    parent.imageURL = url.absoluteString
+                }
+            }
+            parent.dismiss()
+        }
+        
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            parent.dismiss()
+        }
+    }
 }
